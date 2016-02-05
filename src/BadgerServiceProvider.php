09f3b39a5baa9544e13599820cc46b7c7631c6fc
Graham Campbell <graham@alt-three.com>
2016-02-05 14:05:28 +0000

    Removed an unneeded method

--- src/BadgerServiceProvider.php
+++ src/BadgerServiceProvider.php
@@ -26,16 +26,6 @@ use Illuminate\Support\ServiceProvider;
 class BadgerServiceProvider extends ServiceProvider
 {
     /**
-     * Boot the service provider.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
      * Register the service provider.
      *
      * @return void
